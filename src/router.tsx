bd964c911ff827caaa5bb597e551ad57cbadd9e0
은지 <wej0513@dsm.hs.kr>
2024-09-11 16:20:41 +0900

    🎨 :: 온보딩 페이지 퍼블리싱

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,8 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle.style";
 import { LoginPage } from "./Pages/Login";
 import { Home } from "./Pages/Home";
-import { SignupPage } from "./Pages/signup";
+import { SignupPage } from "./Pages/Signup";
+import { OnboardingPage } from "./Pages/OnBoarding";
 
 function router() {
   return (
@@ -12,6 +13,7 @@ function router() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/Signup" element={<SignupPage />} />
+        <Route path="/Onboarding" element={<OnboardingPage />} />
       </Routes>
     </BrowserRouter>
   );
