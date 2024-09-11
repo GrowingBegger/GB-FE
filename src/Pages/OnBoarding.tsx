c8054f8894ab5d96f3bf64278f0685ab34d07fb6
은지 <wej0513@dsm.hs.kr>
2024-09-12 08:38:24 +0900

    ♻️ :: 라우터, 버튼 수정

--- src/Pages/OnBoarding.tsx
+++ src/Pages/OnBoarding.tsx
@@ -4,34 +4,34 @@ import Button from "../Components/Common/Button";
 import { Color } from "../styles/Color";
 
 export const OnboardingPage = () => {
-    return (
-        <Container>
-            <img src={Logo2} alt="로고" />
-            <ButtonWrapper>
-                <Button content="로그인" />
-                <Button
-                    content="회원가입"
-                    backgroundColor="white"
-                    borderColor={Color.color2}
-                    textColor={Color.color2}
-                />
-            </ButtonWrapper>
-        </Container>
-    );
+  return (
+    <Container>
+      <img src={Logo2} alt="로고" />
+      <ButtonWrapper>
+        <Button content="로그인" />
+        <Button
+          content="회원가입"
+          backgroundColor="white"
+          borderColor={Color.color2}
+          textColor={Color.color2}
+        />
+      </ButtonWrapper>
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    display: flex;
-    height: 100vh;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
-    gap: 45vh;
-    padding: 10vw 0px;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 45vh;
+  padding: 10vw 0px;
 `;
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
 `;
--- src/router.tsx
+++ src/router.tsx
@@ -2,22 +2,22 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle.style";
 import { LoginPage } from "./Pages/Login";
 import { Home } from "./Pages/Home";
-import { SignupPage } from "./Pages/signup";
+import { SignupPage } from "./Pages/Signup";
 import { OnboardingPage } from "./Pages/OnBoarding";
 import { BoardDetail } from "./Pages/BoardDetail";
 
 function router() {
-    return (
-        <BrowserRouter>
-            <GlobalStyle />
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/Signup" element={<SignupPage />} />
-                <Route path="/Onboarding" element={<OnboardingPage />} />
-                <Route path="/board/detail" element={<BoardDetail />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Signup" element={<SignupPage />} />
+        <Route path="/Onboarding" element={<OnboardingPage />} />
+        <Route path="/board/detail" element={<BoardDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default router;
