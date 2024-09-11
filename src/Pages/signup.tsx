b1a618e5310e05b137c47bc9d51e5aa746aa62fb
은지 <wej0513@dsm.hs.kr>
2024-09-11 10:25:20 +0900

    🎨 :: 회원가입 퍼블리싱

---
+++ src/Pages/signup.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { Color } from "../styles/Color";
+import { LoginInput } from "../Components/LoginInput";
+import Button from "../Components/Common/Button";
+
+export const SignupPage = () => {
+  return (
+    <Container>
+      <TextWrapper>
+        <Title>회원가입</Title>
+        <Content>계정을 생성해서 앱을 사용해보세요</Content>
+      </TextWrapper>
+      <Wrapper>
+        <LoginInput type="text" placeholder="닉네임을 입력해 주세요." />
+        <LoginInput type="text" placeholder="아이디를 입력해 주세요." />
+        <LoginInput
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+          isPassword
+        />
+        <LoginInput
+          type="text"
+          placeholder="한달 목표 소비 금액을 입력해 주세요."
+          isUnit
+        />
+      </Wrapper>
+      <Button content="회원가입" />
+      <Wrapper>
+        <Signin>
+          이미 회원이신가요?&ensp;<p>로그인 하기</p>
+        </Signin>
+      </Wrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 110px;
+  align-items: center;
+  padding: 30% 30px;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  align-self: flex-start;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+const Title = styled.p`
+  font-family: Pretendard-Semibold;
+  font-size: 25px;
+`;
+
+const Content = styled.p`
+  font-family: Pretendard-Light;
+  font-size: 15px;
+`;
+
+const Signin = styled.p`
+  display: flex;
+  font-family: Pretendard-Regular;
+  font-size: 14px;
+  align-self: center;
+
+  > p {
+    color: ${Color.color2};
+  }
+`;
--- src/router.tsx
+++ src/router.tsx
@@ -2,17 +2,19 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle.style";
 import { LoginPage } from "./Pages/Login";
 import { Home } from "./Pages/Home";
+import { SignupPage } from "./Pages/signup";
 
 function router() {
-    return (
-        <BrowserRouter>
-            <GlobalStyle />
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default router;
