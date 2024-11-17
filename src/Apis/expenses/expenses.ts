e9631014b0527c4acfb6ff8e232b2cf691479b18
euijin <aghpt04@gmail.com>
2024-11-18 08:42:58 +0900

    🐛 :: 지출 그래프 엔드포인트 수정

--- src/Apis/expenses/expenses.ts
+++ src/Apis/expenses/expenses.ts
@@ -2,5 +2,5 @@ import { instance } from "../axios";
 import { getExpensesType } from "./type";
 
 export const getExpenses = async () => {
-    return await instance.get<getExpensesType>("/expenses");
+    return await instance.get<getExpensesType>("/expenses/year");
 };
