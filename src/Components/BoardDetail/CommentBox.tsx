68fb6d15dc683ef7337b015b03f9cb079a6d397a
euijin <aghpt04@gmail.com>
2024-11-19 13:42:54 +0900

    🐛 :: 사진 동그랗게 하기

--- src/Components/BoardDetail/CommentBox.tsx
+++ src/Components/BoardDetail/CommentBox.tsx
@@ -2,62 +2,65 @@ import styled from "styled-components";
 import ProfileIcon from "../../Assets/img/SVG/profileIcon.svg";
 
 interface CommentBoxProps {
-  comment: {
-    content: string;
-    created_at: string;
-    user: {
-      nickname: string;
-      profile: string;
+    comment: {
+        content: string;
+        created_at: string;
+        user: {
+            nickname: string;
+            profile: string;
+        };
     };
-  };
 }
 
 export const CommentBox: React.FC<CommentBoxProps> = ({ comment }) => {
-  return (
-    <Wrapper>
-      <ProfileWrapper>
-        <Profile src={comment.user.profile || ProfileIcon} alt="프로필" />
-        <ProfileWrap>
-          <p style={{ fontFamily: "Pretendard-Medium", fontSize: "15px" }}>
-            {comment.user.nickname}
-          </p>
-          <Date>{comment.created_at.split("T")[0]}</Date>
-        </ProfileWrap>
-      </ProfileWrapper>
-      <Comment>{comment.content}</Comment>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <ProfileWrapper>
+                <Profile backgroundImage={comment.user.profile || ProfileIcon} />
+                <ProfileWrap>
+                    <p style={{ fontFamily: "Pretendard-Medium", fontSize: "15px" }}>{comment.user.nickname}</p>
+                    <Date>{comment.created_at.split("T")[0]}</Date>
+                </ProfileWrap>
+            </ProfileWrapper>
+            <Comment>{comment.content}</Comment>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const ProfileWrapper = styled.div`
-  display: flex;
-  gap: 10px;
-  align-items: center;
+    display: flex;
+    gap: 10px;
+    align-items: center;
 `;
 
 const ProfileWrap = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
-const Profile = styled.img`
-  width: 30px;
+const Profile = styled.div<{ backgroundImage: string }>`
+    width: 30px;
+    height: 30px;
+    background-image: url(${(props) => props.backgroundImage});
+    background-size: cover;
+    background-position: center;
+    border-radius: 50%;
 `;
 
 const Date = styled.p`
-  font-family: Pretendard-Light;
-  font-size: 11px;
-  color: rgba(0, 0, 0, 0.43);
+    font-family: Pretendard-Light;
+    font-size: 11px;
+    color: rgba(0, 0, 0, 0.43);
 `;
 
 const Comment = styled.p`
-  font-family: Pretendard-Regular;
-  margin-left: 40px;
-  margin-top: 10px;
-  width: 340px;
+    font-family: Pretendard-Regular;
+    margin-left: 40px;
+    margin-top: 10px;
+    width: 340px;
 `;
--- src/Components/Home/Board.tsx
+++ src/Components/Home/Board.tsx
@@ -9,124 +9,124 @@ import { Value } from "../../Apis/posts/type";
 import { postDelete } from "../../Apis/posts/posts";
 
 export const Board = ({ value }: { value: Value }) => {
-  const postId = value.postId;
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const postId = value.postId;
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleConfirm = () => {
-    postDelete(postId)
-      .then(() => {
-        console.log("게시글이 삭제 되었습니다.");
-        setIsModalOpen(false);
-        window.location.reload();
-      })
-      .catch(() => {
-        console.log("게시글 삭제 오류");
-      });
-  };
+    const handleConfirm = () => {
+        postDelete(postId)
+            .then(() => {
+                console.log("게시글이 삭제 되었습니다.");
+                setIsModalOpen(false);
+                window.location.reload();
+            })
+            .catch(() => {
+                console.log("게시글 삭제 오류");
+            });
+    };
 
-  return (
-    <>
-      <BoardContainer key={value.postId}>
-        <HeaderWrapper>
-          <ProfileWrapper>
-            <ProfileImg src={value.writerImageUrl} alt="프로필" />
-            <ProfileBox>
-              <Name>{value.writerName}</Name>
-              <Date>{value.createdAt.split("T")[0]}</Date>
-            </ProfileBox>
-          </ProfileWrapper>
-          <IconWrapper>
-            <img
-              src={DeleteIcon}
-              alt="삭제"
-              onClick={() => setIsModalOpen(true)}
-            />
-            <Link to={`/createpost/${value.postId}`}>
-              <img src={EditIcon} alt="수정" />
-            </Link>
-          </IconWrapper>
-        </HeaderWrapper>
-        <PayNotification title={value.title ?? ""} price={value.price ?? 0} />
-        <Link to={`/posts/${value.postId}`}>
-          <Img src={value.imageUrl} alt="게시물 이미지" />
-        </Link>
-        <Link to={`/posts/${value.postId}`}>
-          <Content>{value.content}</Content>
-        </Link>
-      </BoardContainer>
-      {isModalOpen && (
-        <Modal
-          titleText={{ before: "게시물을", after: "하시겠습니까?" }}
-          pointText="삭제"
-          contentText="작성한 게시물이 삭제됩니다"
-          onCancel={() => setIsModalOpen(false)}
-          onConfirm={handleConfirm}
-        />
-      )}
-    </>
-  );
+    return (
+        <>
+            <BoardContainer key={value.postId}>
+                <HeaderWrapper>
+                    <ProfileWrapper>
+                        <ProfileImg backgroundImage={value.writerImageUrl} />
+                        <ProfileBox>
+                            <Name>{value.writerName}</Name>
+                            <Date>{value.createdAt.split("T")[0]}</Date>
+                        </ProfileBox>
+                    </ProfileWrapper>
+                    <IconWrapper>
+                        <img src={DeleteIcon} alt="삭제" onClick={() => setIsModalOpen(true)} />
+                        <Link to={`/createpost/${value.postId}`}>
+                            <img src={EditIcon} alt="수정" />
+                        </Link>
+                    </IconWrapper>
+                </HeaderWrapper>
+                <PayNotification title={value.title ?? ""} price={value.price ?? 0} />
+                <Link to={`/posts/${value.postId}`}>
+                    <Img src={value.imageUrl} alt="게시물 이미지" />
+                </Link>
+                <Link to={`/posts/${value.postId}`}>
+                    <Content>{value.content}</Content>
+                </Link>
+            </BoardContainer>
+            {isModalOpen && (
+                <Modal
+                    titleText={{ before: "게시물을", after: "하시겠습니까?" }}
+                    pointText="삭제"
+                    contentText="작성한 게시물이 삭제됩니다"
+                    onCancel={() => setIsModalOpen(false)}
+                    onConfirm={handleConfirm}
+                />
+            )}
+        </>
+    );
 };
 
 const BoardContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-  background-color: white;
-  gap: 15px;
-  padding: 15px;
-  filter: drop-shadow(4px 4px 10px rgba(0, 0, 0, 0.1));
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    background-color: white;
+    gap: 15px;
+    padding: 15px;
+    filter: drop-shadow(4px 4px 10px rgba(0, 0, 0, 0.1));
 `;
 
 const Img = styled.img`
-  border-radius: 5px;
-  width: 100%;
-  height: 200px;
+    border-radius: 5px;
+    width: 100%;
+    height: 200px;
 `;
 
 const Content = styled.p`
-  font-family: Pretendard-Medium;
-  font-size: 15px;
+    font-family: Pretendard-Medium;
+    font-size: 15px;
 `;
 
 const HeaderWrapper = styled.div`
-  display: flex;
+    display: flex;
 `;
 
 const ProfileWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: auto;
-  gap: 10px;
+    display: flex;
+    align-items: center;
+    margin-right: auto;
+    gap: 10px;
 `;
 
 const ProfileBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3px;
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
 `;
 
-const ProfileImg = styled.img`
-  width: 41px;
-  height: 41px;
+const ProfileImg = styled.div<{ backgroundImage: string }>`
+    width: 41px;
+    height: 41px;
+    background-image: url(${(props) => props.backgroundImage});
+    background-size: cover;
+    background-position: center;
+    border-radius: 50%;
 `;
 
 const Name = styled.p`
-  font-family: Pretendard-Medium;
-  font-size: 15px;
+    font-family: Pretendard-Medium;
+    font-size: 15px;
 `;
 
 const Date = styled.p`
-  font-family: Pretendard-Light;
-  color: rgba(0, 0, 0, 0.43);
-  font-size: 12px;
+    font-family: Pretendard-Light;
+    color: rgba(0, 0, 0, 0.43);
+    font-size: 12px;
 `;
 
 const IconWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  margin-bottom: auto;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-bottom: auto;
 `;
